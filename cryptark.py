0a58fa71c4c8e3ab596cad0d97ba1eb569b2d397
CarterCobb <carter.cobb72@gmail.com>
2022-05-09 17:52:33 -0600

    updated interface

--- cryptark.py
+++ cryptark.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import List
 from log import log
 
 class Cryptark(ABC):
 
     @abstractmethod
-    def encode(self, message: str, encode_args: List[str]) -> str:
+    def encode(self, message: str, encode_args: list[str]) -> str:
         log.error('Not implemented :/')
         pass
 
     @abstractmethod
-    def decode(self, message: str) -> str:
+    def decode(self, message: str, decode_args: list[str]) -> str:
         log.error('Not implemented :/')
         pass
