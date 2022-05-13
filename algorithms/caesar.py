7f641cf52d9671450a8ce26ee95f00a45260b27f
CarterCobb <carter.cobb72@gmail.com>
2022-05-13 10:03:11 -0600

    optomization

--- algorithms/caesar.py
+++ algorithms/caesar.py
@@ -4,22 +4,22 @@ class Caesar(Cryptark):
 
     def encode(self, message: str, encode_args: list[str]) -> str:
         message = ''.join(filter(str.isalnum, message)).upper()
-        key = int(encode_args[0])
-        alphabet = [chr(i + 65) for i in range(0,26)]
-        cipher = self._shift(alphabet, key)
-        print(alphabet)
-        print(cipher)
+        alphabet, cipher = self._main_logic(message, encode_args)
         return ''.join([cipher[alphabet.index(l)] for l in message])
 
     def decode(self, message: str, decode_args: list[str]) -> str:
         message = ''.join(filter(str.isalnum, message)).upper()
-        key = int(decode_args[0])
-        alphabet = [chr(i + 65) for i in range(0,26)]
-        cipher = self._shift(alphabet, key)
-        print(alphabet)
-        print(cipher)
+        alphabet, cipher = self._main_logic(message, decode_args)
         return ''.join([alphabet[cipher.index(l)] for l in message])
 
     def _shift(self, seq, n):
         n = n % len(seq)
         return seq[n:] + seq[:n]
+
+    def _main_logic(self, message, args):
+        key = int(args[0])
+        alphabet = [chr(i + 65) for i in range(0,26)]
+        cipher = self._shift(alphabet, key)
+        print(alphabet)
+        print(cipher) 
+        return alphabet, cipher
