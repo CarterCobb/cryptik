0f011b92ec2d0284ce2a40ab13256bd90e71bcbd
CarterCobb <carter.cobb72@gmail.com>
2022-05-13 09:54:57 -0600

    added logging

--- algorithms/caesar.py
+++ algorithms/caesar.py
@@ -4,16 +4,20 @@ class Caesar(Cryptark):
 
     def encode(self, message: str, encode_args: list[str]) -> str:
         message = ''.join(filter(str.isalnum, message)).upper()
-        key = int(encode_args[0]) - 1
+        key = int(encode_args[0])
         alphabet = [chr(i + 65) for i in range(0,26)]
-        cipher = self._shift(alphabet, key + 1)
+        cipher = self._shift(alphabet, key)
+        print(alphabet)
+        print(cipher)
         return ''.join([ cipher[alphabet.index(l)] for l in message])
 
     def decode(self, message: str, decode_args: list[str]) -> str:
         message = ''.join(filter(str.isalnum, message)).upper()
-        key = int(decode_args[0]) - 1
+        key = int(decode_args[0])
         alphabet = [chr(i + 65) for i in range(0,26)]
-        cipher = self._shift(alphabet, key + 1)
+        cipher = self._shift(alphabet, key)
+        print(alphabet)
+        print(cipher)
         return ''.join([ alphabet[cipher.index(l)] for l in message])
 
     def _shift(self, seq, n):
