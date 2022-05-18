6e09152378cfd5cf8db9ebfeddc65ffaffaade7f
CarterCobb <carter.cobb72@gmail.com>
2022-05-18 09:13:08 -0600

    added affine cipher

--- algorithms/multiplicative.py
+++ algorithms/multiplicative.py
@@ -1,12 +1,14 @@
+from audioop import mul
 from cryptark import Cryptark
 from log import log
+from algorithms.caesar import Caesar
 
 """
 Key pairs:
 {1,1}
-{3,9}
 {5,21}
 {7,15}
+{9,3}
 {11,19}
 {17,23}
 {25,25}
@@ -34,3 +36,33 @@ class Multiplicative(Cryptark):
         print('plain: ', alphabet)
         print('cipher:',cipher) 
         return alphabet, cipher
+
+
+class Affine(Cryptark):
+
+    def encode(self, message: str, encode_args: list[str]) -> str:
+        message = ''.join(filter(str.isalnum, message)).upper()
+        alphabet, cipher = self._main_logic(encode_args)
+        return ''.join([cipher[alphabet.index(l)] for l in message])
+
+    def decode(self, message: str, decode_args: list[str]) -> str:
+        message = ''.join(filter(str.isalnum, message)).upper()
+        alphabet, cipher = self._main_logic(decode_args)
+        return ''.join([alphabet[cipher.index(l)] for l in message])
+    
+    def _main_logic(self, args):
+        main_key = int(args[0])
+        multi = int(main_key / 26)
+        shift = main_key % 26
+        print(multi, shift)
+        if multi % 2 == 0 or multi == 13:
+            log.error('Not relatively prime to the mod (26) or key is 13; cannot be a common factor')
+            return [chr(i + 65) for i in range(0,26)], [chr(i + 65) for i in range(0,26)]
+        alphabet = [chr(i + 65) for i in range(0,26)]
+        print('plain: ', alphabet)
+        cipher = [chr(((i * multi) % 26) + 65) for i in range(0, 26)]
+        print(f'mulit{multi}:', cipher)
+        cipher = [alphabet[((i * multi) + shift) % 26] for i in range(0, 26)]
+        print('cipher:',cipher) 
+        return alphabet, cipher
+
