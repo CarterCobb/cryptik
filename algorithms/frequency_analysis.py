133ce5f15297129ba8c5185423fc23d288c0c09a
CarterCobb <carter.cobb72@gmail.com>
2022-05-22 14:59:13 -0600

    implemented freq analysis

--- algorithms/frequency_analysis.py
+++ algorithms/frequency_analysis.py
@@ -1,7 +1,8 @@
-from operator import index
 from cryptark import Cryptark
+import re
+from log import log
 
-WEIGHTS = ['E','T','A','O','I','N','S','H','R','D','L','C','U','M','W','F','G','Y','P','B','V','K','J','X','Q','Z']
+WEIGHTS = ['E','T','A','O','I','N','S','H','R','D','L','C','U','M','W','F','G','Y','P','B','V','K','J','X','Q','Z'] # Most common weight 
 
 class FrequencyAnalysis(Cryptark):
 
@@ -9,21 +10,35 @@ class FrequencyAnalysis(Cryptark):
         return super().encode(message, encode_args)
 
     def decode(self, message: str, decode_args: list[str]) -> str:
-        message = ''.join(filter(str.isalnum, message)).upper()
+        message = ''.join(re.sub(r'[^a-zA-Z\s]', u'', message, flags=re.UNICODE).upper().splitlines()) 
         print('Length:',len(message))
         alpha_map = [{f'{chr(i + 65)}': {'count': 0, 'percent': 0}} for i in range(0, 26)]
         for l in message:
-            i = 0
-            for i_ in range(0, len(alpha_map)):
-                if l in alpha_map[i_]: i = i_
-            alpha_map[i][l]['count'] = alpha_map[i][l]['count'] + 1
-            alpha_map[i][l]['percent'] = (alpha_map[i][l]['count'] / len(message)) * 100
+            if not l.isspace():
+                i = 0
+                for i_ in range(0, len(alpha_map)):
+                    if l in alpha_map[i_]: i = i_
+                alpha_map[i][l]['count'] = alpha_map[i][l]['count'] + 1
+                alpha_map[i][l]['percent'] = (alpha_map[i][l]['count'] / len(message)) * 100
         res = sorted(alpha_map, key = lambda i: list(i.values())[0]['count'], reverse=True)
-        print('general: ', WEIGHTS)
         final_map = [list(i.keys())[0] for i in res]
-        print('proposed:',final_map)
+        print('frequencies:',final_map)
+        log.info(f'MSSAGE: {message}')  
+        self._calculate(message, final_map)
+        while True:
+            shuffle = input("Swap letters? ['L1 L2' to swap] ['done' to end]\r\n-> ")
+            if shuffle.lower().strip() == 'done': break
+            letters = shuffle.upper().split(' ')
+            i1, i2 = WEIGHTS.index(letters[0]), WEIGHTS.index(letters[1])
+            WEIGHTS[i1], WEIGHTS[i2] = WEIGHTS[i2], WEIGHTS[i1]
+            self._calculate(message, final_map)
+        return f"\r\nplain:  {' '.join(WEIGHTS)}\r\ncipher: {' '.join(final_map)}"
+
+    def _calculate(self, message, final_map): 
         formatted_message = ''
         for l in message:
-            formatted_message += WEIGHTS[final_map.index(l)]
-        print(formatted_message)
-        return super().decode(message, decode_args)
+            if not l.isspace():
+                formatted_message += WEIGHTS[final_map.index(l)]
+            else: formatted_message += l
+        log.success(formatted_message)
+        
