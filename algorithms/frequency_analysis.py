21eceddb6e088131c0c189f86e976f9b2423d01f
CarterCobb <carter.cobb72@gmail.com>
2022-05-23 11:10:43 -0600

    bug fixes and logs

--- algorithms/frequency_analysis.py
+++ algorithms/frequency_analysis.py
@@ -21,18 +21,26 @@ class FrequencyAnalysis(Cryptark):
                 alpha_map[i][l]['count'] = alpha_map[i][l]['count'] + 1
                 alpha_map[i][l]['percent'] = (alpha_map[i][l]['count'] / len(message)) * 100
         res = sorted(alpha_map, key = lambda i: list(i.values())[0]['count'], reverse=True)
+        for m in res: print(m)
         final_map = [list(i.keys())[0] for i in res]
         print('frequencies:',final_map)
         log.info(f'MSSAGE: {message}')  
         self._calculate(message, final_map)
         while True:
-            shuffle = input("Swap letters? ['L1 L2' to swap] ['done' to end]\r\n-> ")
-            if shuffle.lower().strip() == 'done': break
-            letters = shuffle.upper().split(' ')
-            i1, i2 = WEIGHTS.index(letters[0]), WEIGHTS.index(letters[1])
-            WEIGHTS[i1], WEIGHTS[i2] = WEIGHTS[i2], WEIGHTS[i1]
-            self._calculate(message, final_map)
-        return f"\r\nplain:  {' '.join(WEIGHTS)}\r\ncipher: {' '.join(final_map)}"
+            try:
+                shuffle = input("Swap letters? ['L1 L2' to swap] ['done' to end]\r\n-> ")
+                if shuffle.lower().strip() == 'done': break
+                letters = shuffle.upper().split(' ')
+                i1, i2 = WEIGHTS.index(letters[0]), WEIGHTS.index(letters[1])
+                WEIGHTS[i1], WEIGHTS[i2] = WEIGHTS[i2], WEIGHTS[i1]
+                self._calculate(message, final_map)
+                letter_map = dict(zip(WEIGHTS, final_map)) 
+                sorted_letter_map = {key:letter_map[key] for key in sorted(letter_map)}
+                print(f"\r\nplain:  {' '.join(sorted_letter_map.keys())}\r\ncipher: {' '.join([sorted_letter_map[i] for i in sorted_letter_map.keys()])}")
+            except: print("Invlaid Try again")
+        letter_map = dict(zip(WEIGHTS, final_map)) 
+        sorted_letter_map = {key:letter_map[key] for key in sorted(letter_map)}
+        return f"\r\nplain:  {' '.join(sorted_letter_map.keys())}\r\ncipher: {' '.join([sorted_letter_map[i] for i in sorted_letter_map.keys()])}"
 
     def _calculate(self, message, final_map): 
         formatted_message = ''
@@ -40,5 +48,6 @@ class FrequencyAnalysis(Cryptark):
             if not l.isspace():
                 formatted_message += WEIGHTS[final_map.index(l)]
             else: formatted_message += l
+
         log.success(formatted_message)
         
